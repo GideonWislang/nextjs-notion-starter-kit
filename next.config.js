5438def316c1b1c025824967f2b501b23d913f6a
Gideon Wislang <hello@gideonwislang.com>
2023-08-26 00:20:43 +1200

    dsdssds

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
 
-module.exports = withBundleAnalyzer({
   async redirects() {
     return [
       {
@@ -22,4 +24,4 @@ module.exports = withBundleAnalyzer({
   future: {
     webpack5: true
   }
-})
+}
